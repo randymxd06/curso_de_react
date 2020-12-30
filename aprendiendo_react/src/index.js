cccd7ef97d1eb02d104cc848852c6a749434806f
Randy R. Martínez <55677075+randymxd06@users.noreply.github.com>
2020-12-30 18:27:19 -0400

    elimine todo y lo hice de nuevo

--- aprendiendo_react/src/index.js
+++ aprendiendo_react/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const nombre = 'Randy';
-const color = 'blue'
+const color = 'blue';
+
 const jsx = (
   <>
-    <h1 className="nombre_de_clase" style={{color: color}}>Hola {nombre}</h1>
-    <p>Que tengas buen día</p>
+    <h1 className="titulo" style={{color: color}}>Hola {nombre}</h1>
+    <p></p>
   </>
 );
-
 ReactDOM.render(jsx,document.getElementById('root'));
