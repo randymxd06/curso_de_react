7f5811fcf3b9a82fdc6f4d035eab996c738d21c2
Randy R. Martínez <55677075+randymxd06@users.noreply.github.com>
2020-12-30 18:47:48 -0400

    hice validaciones en react

--- aprendiendo_react/src/index.js
+++ aprendiendo_react/src/index.js
@@ -3,11 +3,36 @@ import ReactDOM from 'react-dom';
 
 const nombre = 'Randy';
 const color = 'blue';
+const sesion = true;
+const pais = 'República Dominicana';
 
 const jsx = (
-  <>
-    <h1 className="titulo" style={{color: color}}>Hola {nombre}</h1>
-    <p></p>
+  
+  <>{/*Para mostrar contenido debemos poner todo dentro de un div o podemos poner todo dentro de las flechas <> </>*/}
+    {/*Le pregunto que si sesion es true me muestre su contenido (caso verdadero ?) sino que muestre un parrafo que diga que no ha iniciado sesion (caso contrario :)*/}
+    {sesion === true ?
+    <div>
+      {/*Las clases se ponen con className, los style ven dentro de comillas cada propiedad*/}
+      <h1 className="titulo" style={{color: color}}>Hola {nombre}</h1>
+      <p>Que tengas un buen día</p>
+      {/*Le pregunto que si país existe que muestre el párrafo con el país*/}
+      {pais && <p>Tu eres de {pais}</p>}
+    </div>
+    : 
+    <p>No has iniciado sesion</p>}
+    
   </>
 );
+
+//Esta es otra manera de hacer la validación de si la sesion es true, solo que abajo hay que poner el nombre de la función en vez de la variable jsx//
+/*
+const verificarSesion = (sesion) => {
+  if(sesion === true){
+    return jsx;
+  } else {
+    return <h1>No has iniciado sesion</h1>
+  }
+}
+*/
+
 ReactDOM.render(jsx,document.getElementById('root'));
