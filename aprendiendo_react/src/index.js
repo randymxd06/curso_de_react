38d1c075031b2649516551ddfed0f6a0269672b7
Randy R. Martínez <55677075+randymxd06@users.noreply.github.com>
2021-01-11 12:27:31 -0400

    Cree mas componentes

--- aprendiendo_react/src/index.js
+++ aprendiendo_react/src/index.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Card from './components/card'
+import React from "react";
+import ReactDOM from "react-dom";
+// import Card from "./components/card";
+// import Welcome from "./components/welcome";
+import Exercises from './pages/exercises'
+import "bootstrap/dist/css/bootstrap.css";
 
-const nombre = 'Randy';
-const color = 'blue';
-const sesion = true;
-const pais = 'República Dominicana';
+// const nombre = "Randy";
+// const color = "blue";
+// const sesion = true;
+// const pais = "República Dominicana";
 
-const jsx = (
-  
-  <>{/*Para mostrar contenido debemos poner todo dentro de un div o podemos poner todo dentro de las flechas <> </>*/}
-    {/*Le pregunto que si sesion es true me muestre su contenido (caso verdadero ?) sino que muestre un parrafo que diga que no ha iniciado sesion (caso contrario :)*/}
-    {sesion === true ?
-    <div>
-      {/*Las clases se ponen con className, los style ven dentro de comillas cada propiedad*/}
-      <h1 className="titulo" style={{color: color}}>Hola {nombre}</h1>
-      <p>Que tengas un buen día</p>
-      {/*Le pregunto que si país existe que muestre el párrafo con el país*/}
-      {pais && <p>Tu eres de {pais}</p>}
-    </div>
-    : 
-    <p>No has iniciado sesion</p>}
-  </>
-);
+// const jsx = (
+//   <>
+//     {/*Para mostrar contenido debemos poner todo dentro de un div o podemos poner todo dentro de las flechas <> </>*/}
+//     {/*Le pregunto que si sesion es true me muestre su contenido (caso verdadero ?) sino que muestre un parrafo que diga que no ha iniciado sesion (caso contrario :)*/}
+//     {sesion === true ? (
+//       <div>
+//         {/*Las clases se ponen con className, los style ven dentro de comillas cada propiedad*/}
+//         <h1 className="titulo" style={{ color: color }}>
+//           Hola {nombre}
+//         </h1>
+//         <p>Que tengas un buen día</p>
+//         {/*Le pregunto que si país existe que muestre el párrafo con el país*/}
+//         {pais && <p>Tu eres de {pais}</p>}
+//       </div>
+//     ) : (
+//       <p>No has iniciado sesion</p>
+//     )}
+//   </>
+// );
 //Esta es otra manera de hacer la validación de si la sesion es true, solo que abajo hay que poner el nombre de la función en vez de la variable jsx//
 /*
 const verificarSesion = (sesion) => {
@@ -34,4 +40,7 @@ const verificarSesion = (sesion) => {
 }
 */
 
-ReactDOM.render(<Card/>,document.getElementById('root'));
+const container = document.getElementById("root");
+
+//ReactDOM.render(_____QUÉ_____DÓNDE_____);
+ReactDOM.render(<Exercises/>, container);
